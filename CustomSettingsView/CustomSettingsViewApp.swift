a5ed2edd65b82d36b39412114aa36dbd3a8d4001
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-15 10:34:17 +0200

    Initial commit

---
+++ CustomSettingsView/CustomSettingsViewApp.swift
@@ -0,0 +1,19 @@
+//
+//  CustomSettingsViewApp.swift
+//  CustomSettingsView
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CustomSettingsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
